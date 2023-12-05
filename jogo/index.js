49ee15298944623e83a2b42cbd4358109b2c600f
jeanpcoelho <jeanpc0018@gmail.com>
2023-12-05 11:52:19 -0300

    update questions

---
+++ jogo/index.js
@@ -0,0 +1,61 @@
+let currentQuestionIndex = 0;
+
+function startGame() {
+    showQuestion();
+}
+
+function showQuestion() {
+    const questionContainer = document.getElementById('question-container');
+    const optionsContainer = document.getElementById('options-container');
+    const nextButton = document.getElementById('next-button');
+
+    const currentQuestion = questions[currentQuestionIndex];
+
+    questionContainer.innerText = currentQuestion.question;
+    optionsContainer.innerHTML = '';
+
+    currentQuestion.options.forEach((option, index) => {
+        const optionElement = document.createElement('div');
+        optionElement.className = 'option';
+        optionElement.innerText = `${index + 1}. ${option}`;
+        optionElement.onclick = () => checkAnswer(option);
+        optionsContainer.appendChild(optionElement);
+    });
+
+    nextButton.style.display = 'none';
+
+    if (currentQuestionIndex === questions.length - 1) {
+        nextButton.innerText = 'Finalizar Jogo';
+    }
+
+    if (currentQuestionIndex < questions.length - 1) {
+        nextButton.style.display = 'none';
+    } else {
+        nextButton.style.display = 'block';
+    }
+}
+
+function checkAnswer(selectedOption) {
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (selectedOption === currentQuestion.answer) {
+        alert('Resposta Correta!');
+    } else {
+        alert('Resposta Incorreta!');
+    }
+
+    const nextButton = document.getElementById('next-button');
+    nextButton.style.display = 'block';
+}
+
+function nextQuestion() {
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        alert('Parabéns! Você completou o jogo do Show do Milhão.');
+    }
+}
+
+startGame();
---
+++ jogo/questions.js
@@ -0,0 +1,77 @@
+const questions = [
+    {
+        question: "Qual a capital do Brasil ?",
+        options: ["Brasilia","Belo Horizonte","Salvador","Sao Paulo"],
+        answer: "Brasilia"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual desses numeros e primo ?",
+        options: ["3", "6", "10", "8"],
+        answer: "3"
+    },
+    {
+        question: "Qual a data comemora a independencia do Brasil ?",
+        options: ["25 de dezembro", "15 de novembro", "12 de novembro", "7 de setembro"],
+        answer: "7 de setembro"
+    },
+    {
+        question: "quantos dias sao nescessarios para terra orbiar o sol ?",
+        options: ["380 dias", "365 dias", "390 dias", "388 dias"],
+        answer: "365 dias"
+    },
+    {
+        question: "Quantos estados tem o Brasil contando o destrito federal ?",
+        options: ["30", "27", "25", "29"],
+        answer: "27"
+    },
+    {
+        question: "qual a materia prima para fabricaçao do plastico",
+        options: ["madeira", "areia", "niobio", "petroleo"],
+        answer: "petroleo"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "Qual é a capital da França?",
+        options: ["Berlim", "Londres", "Paris", "Madrid"],
+        answer: "Paris"
+    },
+];
