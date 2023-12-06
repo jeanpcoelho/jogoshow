83376068d36ed6b5ba94da61535f6893b24776c1
jeanpcoelho <jeanpc0018@gmail.com>
2023-12-06 09:06:51 -0300

    Update questions.js
    add rodadas de perguntas

--- jogo/questions.js
+++ jogo/questions.js
@@ -1,4 +1,5 @@
 const questions = [
+    [
     {
         question: "Qual a capital do Brasil ?",
         options: ["Brasilia","Belo Horizonte","Salvador","Sao Paulo"],
@@ -14,6 +15,8 @@ const questions = [
         options: ["3", "6", "10", "8"],
         answer: "3"
     },
+],
+[
     {
         question: "Qual a data comemora a independencia do Brasil ?",
         options: ["25 de dezembro", "15 de novembro", "12 de novembro", "7 de setembro"],
@@ -29,16 +32,25 @@ const questions = [
         options: ["30", "27", "25", "29"],
         answer: "27"
     },
+],
+[
     {
         question: "qual a materia prima para fabricaçao do plastico",
         options: ["madeira", "areia", "niobio", "petroleo"],
         answer: "petroleo"
     },
     {
+        question: "Qual o maior planeta so sistema solar?",
+        options: ["terra", "marte", "venus", "jupiter"],
+        answer: "jupiter"
+    },
+    {
         question: "Qual é a capital da França?",
         options: ["Berlim", "Londres", "Paris", "Madrid"],
         answer: "Paris"
     },
+],
+[
     {
         question: "Qual é a capital da França?",
         options: ["Berlim", "Londres", "Paris", "Madrid"],
@@ -54,6 +66,8 @@ const questions = [
         options: ["Berlim", "Londres", "Paris", "Madrid"],
         answer: "Paris"
     },
+],
+[
     {
         question: "Qual é a capital da França?",
         options: ["Berlim", "Londres", "Paris", "Madrid"],
@@ -69,9 +83,5 @@ const questions = [
         options: ["Berlim", "Londres", "Paris", "Madrid"],
         answer: "Paris"
     },
-    {
-        question: "Qual é a capital da França?",
-        options: ["Berlim", "Londres", "Paris", "Madrid"],
-        answer: "Paris"
-    },
+],
 ];
